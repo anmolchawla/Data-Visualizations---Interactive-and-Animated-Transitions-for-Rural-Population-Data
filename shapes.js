84b9f5b66fbb1f8bdca1425b2c9e6a345c7a2f3c
anmolchawla <anmolcha@usc.edu>
2018-10-08 01:16:11 -0700

    second commit
    check

--- shapes.js
+++ shapes.js
@@ -1,11 +1,69 @@
 // Axes, axes labels, tick marks, tickmark labels.
+var holder;
+var temp;
+var x;
+var barc;
+var dekhta;
+var dekhta1;
+
+function compare(a,b) {
+  if (a.Country_Name < b.Country_Name)
+    return -1;
+  if (a.Country_Name > b.Country_Name)
+    return 1;
+  return 0;
+}
+
+function reset_countries(){
+holder = temp;
+console.log(holder)
+update(holder);
+
+}
+
+function top_5(){
+const top = holder.map(x => x);
+top.sort(function(x,y){return d3.descending(x.Percentage_Rural_Population_2017,y.Percentage_Rural_Population_2017);});
+top.splice(5,5);
+console.log(top)
+update(top);
+}
+
+function down_5(){
+var down = holder.map(x=>x);
+down.sort(function(x,y){return d3.ascending(x.Percentage_Rural_Population_2017,y.Percentage_Rural_Population_2017);});
+down.splice(5,5);
+console.log(down)
+update(down);
+}
+
+function reset_alpha(){
+holder.sort(compare);
+console.log(holder);
+update(holder);
+}
+
+function ascen(){
+holder.sort(function(x,y){return d3.ascending(x.Percentage_Rural_Population_2017,y.Percentage_Rural_Population_2017);});
+console.log(holder);
+update(holder);
+}
+
+
+function descen(){
+holder.sort(function(x,y){return d3.descending(x.Percentage_Rural_Population_2017,y.Percentage_Rural_Population_2017);});
+console.log(holder);
+update(holder);
+}
+
 
 
 var barchart = function(data){
 
 
 var color = d3.scaleOrdinal(d3.schemeCategory10);	
-var barc = d3.select('#bar')
+barc = d3.select('#bar')
+
 	
 
 
@@ -14,13 +72,14 @@ var barc = d3.select('#bar')
 
 var width = 900, height = 1100;
 
-var x = d3.scalePoint()
+x = d3.scalePoint()
 		.range([5,width-10]);
 
 x.domain(data.map(function(d){return d.Country_Name;}));
-barc.append("g")
-		.attr("transform","translate(70," + 560 + ")")
-		.call(d3.axisBottom(x))
+
+dekhta = barc.append("g")
+dekhta.attr("transform","translate(70," + 560 + ")")
+	.call(d3.axisBottom(x))
 
 
 var y = d3.scaleLinear()
@@ -32,12 +91,11 @@ barc.append("g")
 .attr("transform","translate(55," + 0 + ")")
 .call(d3.axisLeft(y));
 
-
 barc.selectAll('rect')
-	.data(data)
-	.enter()
-    .append('rect')
-	.attr('x', function(d) { return (x(d.Country_Name)+55); })
+    .data(data)
+    .enter()
+	.append('rect')
+    .attr('x', function(d) { return (x(d.Country_Name)+55); })
 	.attr('y', function (d) { return (600 - (600 - y(d.Percentage_Rural_Population_2017))); })
 	.attr('width', function (d) { return [30]; })
 	.attr('height', function (d) { return (560 - y(d.Percentage_Rural_Population_2017)); })
@@ -65,9 +123,35 @@ return ;
 }
 
 
+function update(just){
+	var next = barc.selectAll("rect")
+	next.enter().data(just)
+
+	x.domain(just.map(function(d){return d.Country_Name;}));
+    
+
+
+       next.transition()
+    	   .duration(3000)
+		   .attr('x',function(d) { return (x(d.Country_Name)+55); })
+
+	dekhta.transition()
+		.duration(3000)
+		.attr("transform","translate(70," + 560 + ")")
+		.call(d3.axisBottom(x))
+
+	//next.exit().remove();
+
+		
+}
+
+
 
 d3.json("world.json").then(function (data) { 
   //console.log(data);
   barchart(data);
+  holder = data.map(x=>x);
+  temp = data.map(x=>x);
+  console.log(holder);
 });
 
